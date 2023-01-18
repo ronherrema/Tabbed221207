ff781f1b3d3b9ba8677c01a92d421a917b3876bf
Ron Herrema <rherrema@gmail.com>
2023-01-18 17:42:32 +0000

    useReState and Stack nav working

--- Stack.js
+++ Stack.js
@@ -1,44 +1,56 @@
 import "react-native-gesture-handler"
 import * as React from "react"
-import { Button, StyleSheet, View } from "react-native"
-import { NavigationContainer } from "@react-navigation/native"
+import {
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  Image,
+} from "react-native"
 import { createStackNavigator } from "@react-navigation/stack"
 
 const Stack = createStackNavigator()
 
 function Screen1({ navigation }) {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "red",
-        justifyContent: "space-evenly",
-      }}
-    >
-      <Button
-        title="Thing 2"
-        onPress={() => navigation.navigate("Thing 2")}
-      ></Button>
-      <Button
-        title="Thing 3"
-        onPress={() => navigation.navigate("Thing 3")}
-      ></Button>
+    <View style={styles.stack}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Item 1")}
+        style={styles.button}
+      >
+        <Text style={{ fontSize: 28 }}>Item 1</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Item 2")}
+        style={styles.button}
+      >
+        <Text style={{ fontSize: 28 }}>Item 2</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 function Screen2() {
-  return <View style={{ flex: 1, backgroundColor: "blue" }}></View>
+  return (
+    <View style={[styles.container, { backgroundColor: "#77777755" }]}>
+      <Image source={require("./assets/blue.png")}></Image>
+    </View>
+  )
 }
 function Screen3() {
-  return <View style={{ flex: 1, backgroundColor: "green" }}></View>
+  return (
+    <View style={[styles.container, { backgroundColor: "#77777755" }]}>
+      <Image source={require("./assets/apple.png")}></Image>
+    </View>
+  )
 }
 
 export default function StackScreen() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Thing 1" component={Screen1}></Stack.Screen>
-      <Stack.Screen name="Thing 2" component={Screen2}></Stack.Screen>
-      <Stack.Screen name="Thing 3" component={Screen3}></Stack.Screen>
+      <Stack.Screen name="Item List" component={Screen1}></Stack.Screen>
+      <Stack.Screen name="Item 1" component={Screen2}></Stack.Screen>
+      <Stack.Screen name="Item 2" component={Screen3}></Stack.Screen>
     </Stack.Navigator>
   )
 }
@@ -49,5 +61,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    width: "40%",
+    height: "15%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: "cornflowerblue",
+  },
+  stack: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "space-evenly",
+    alignItems: "center",
   },
 })
